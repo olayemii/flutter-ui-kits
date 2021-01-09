dfffef4263af9c33ec56ea2254b9f60979c579bf
Olayemii <donyemisco@gmail.com>
2021-01-10 00:19:36 +0100

    fix overflow

--- language_learning_ui/lib/widgets/course_card.dart
+++ language_learning_ui/lib/widgets/course_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:language_learning_ui/constants.dart';
 import 'package:language_learning_ui/models/course_model.dart';
 
 class CourseCard extends StatelessWidget {
@@ -7,19 +8,33 @@ class CourseCard extends StatelessWidget {
   CourseCard({this.course});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
-            height: ScreenUtil().setHeight(120.0),
-            width: ScreenUtil().setWidth(145.0),
-            decoration: BoxDecoration(
-              color: this.course.color,
-              borderRadius: BorderRadius.circular(8.0),
+    return Container(
+      height: ScreenUtil().setHeight(120.0),
+      width: ScreenUtil().setWidth(145.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: this.course.color,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            this.course.name,
+            style: TextStyle(
+              color: Constants.primaryTextColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 14.0,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
