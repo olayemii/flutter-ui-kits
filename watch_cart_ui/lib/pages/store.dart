491fa167de99ffe875696ed11987b3a59800a820
Olayemii <donyemisco@gmail.com>
2021-01-10 10:25:47 +0100

    fix home

--- watch_cart_ui/lib/pages/store.dart
+++ watch_cart_ui/lib/pages/store.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:watch_cart_ui/constants.dart';
@@ -13,18 +14,137 @@ class Store extends StatelessWidget {
           child: Container(
             padding: EdgeInsets.symmetric(horizontal: 16.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisSize: MainAxisSize.min,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     IconButton(
-                      icon: SvgPicture.asset("assets/menu.svg"),
+                      icon: SvgPicture.asset(
+                        "assets/svg/menu.svg",
+                        width: 12.0,
+                        height: 12.0,
+                      ),
                       onPressed: () {},
                     ),
                     IconButton(
-                      icon: SvgPicture.asset("assets/hamburger.svg"),
+                      icon: SvgPicture.asset(
+                        "assets/svg/hamburger.svg",
+                        width: 16.0,
+                        height: 16.0,
+                      ),
                       onPressed: () {},
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                RichText(
+                  textAlign: TextAlign.left,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "World\n",
+                        style: TextStyle(
+                          height: 2.5,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromRGBO(34, 34, 34, 1),
+                        ),
+                      ),
+                      TextSpan(
+                        text: "of luxury",
+                        style: TextStyle(
+                          fontSize: 28.0,
+                          color: Color.fromRGBO(34, 34, 34, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(right: 15.0),
+                        width: 60.0,
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          border: Border.all(
+                            color: Color.fromRGBO(230, 230, 230, 1),
+                          ),
+                        ),
+                        child: Icon(FlutterIcons.search_fea),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(right: 15.0),
+                        width: 60.0,
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          border: Border.all(
+                            color: Color.fromRGBO(230, 230, 230, 1),
+                          ),
+                        ),
+                        child: Icon(FlutterIcons.md_options_ion),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(right: 15.0),
+                        width: 120.0,
+                        height: 60.0,
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        decoration: BoxDecoration(
+                          color: Constants.primaryColor,
+                          borderRadius: BorderRadius.circular(8.0),
+                          border: Border.all(
+                            color: Color.fromRGBO(230, 230, 230, 1),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Cart",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14.0,
+                              ),
+                            ),
+                            Container(
+                              width: 30.0,
+                              height: 30.0,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "2",
+                                  style: TextStyle(
+                                    color: Constants.primaryColor,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14.0,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     )
                   ],
                 )
