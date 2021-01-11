a06e779a7dbb550328db6366a177bf57b3790d91
Olayemii <donyemisco@gmail.com>
2021-01-11 17:08:45 +0100

    complete property card

--- real_estate_ui/lib/widgets/property_card.dart
+++ real_estate_ui/lib/widgets/property_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:real_estate_ui/utils/constants.dart';
 
 class PropertyCard extends StatelessWidget {
   @override
@@ -9,15 +11,78 @@ class PropertyCard extends StatelessWidget {
       child: Column(
         children: [
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(
-                    "assets/images/property-1.png",
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(18.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      "assets/images/property-1.png",
+                    ),
                   ),
                 ),
               ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Gorgious Appartment #1215",
+                        style: TextStyle(
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "\$2,800/yr",
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        color: Constants.primaryColor,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "1-4 Beds, 1-2 Baths, 1,852 sqft",
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    color: Color(0xFF343434),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      FlutterIcons.map_pin_fea,
+                      size: 15.0,
+                      color: Color.fromRGBO(255, 136, 0, 1),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Text(
+                      "252 1st Avenue",
+                      style: TextStyle(
+                        fontSize: 13.0,
+                        color: Color(0xFF343434),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           )
         ],
