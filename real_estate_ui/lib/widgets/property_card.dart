7d1dd11942df4b00caae9bc99cbeb760f73aeda3
Olayemii <donyemisco@gmail.com>
2021-01-11 16:52:49 +0100

    fit

--- real_estate_ui/lib/widgets/property_card.dart
+++ real_estate_ui/lib/widgets/property_card.dart
@@ -6,6 +6,22 @@ class PropertyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: ScreenUtil().setHeight(250.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    "assets/images/property-1.png",
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
