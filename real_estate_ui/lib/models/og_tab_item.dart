a455eb5d4bf4fab1bf8f8206caa92135d6e0c607
Olayemii <donyemisco@gmail.com>
2021-01-11 15:35:59 +0100

    complete og tab

--- real_estate_ui/lib/models/og_tab_item.dart
+++ real_estate_ui/lib/models/og_tab_item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class OgTabItem {
   final String title;
-  final Widget page;
 
-  OgTabItem({this.title, this.page});
+  OgTabItem({this.title});
 }
--- real_estate_ui/lib/widgets/og_tab.dart
+++ real_estate_ui/lib/widgets/og_tab.dart
@@ -3,40 +3,54 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:real_estate_ui/models/og_tab_item.dart';
 import 'package:real_estate_ui/utils/constants.dart';
 
-class OgTab extends StatelessWidget {
+class OgTab extends StatefulWidget {
   final List<OgTabItem> items;
   OgTab({this.items});
+
+  @override
+  _OgTabState createState() => _OgTabState();
+}
+
+class _OgTabState extends State<OgTab> {
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         children: List.generate(
-          3,
+          widget.items.length,
           (index) => Expanded(
-            child: Container(
-              height: ScreenUtil().setHeight(47.0),
-              decoration: BoxDecoration(
-                color: index == 0
-                    ? Color.fromRGBO(230, 236, 254, 1)
-                    : Colors.transparent,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(index == 0 ? 5 : 0),
-                  bottomLeft: Radius.circular(index == 0 ? 5 : 0),
-                  topRight: Radius.circular(index == 2 ? 5 : 0),
-                  bottomRight: Radius.circular(index == 2 ? 5 : 0),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  activeIndex = index;
+                });
+              },
+              child: Container(
+                height: ScreenUtil().setHeight(47.0),
+                decoration: BoxDecoration(
+                  color: activeIndex == 0
+                      ? Color.fromRGBO(230, 236, 254, 1)
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(index == 0 ? 5 : 0),
+                    bottomLeft: Radius.circular(index == 0 ? 5 : 0),
+                    topRight: Radius.circular(index == 2 ? 5 : 0),
+                    bottomRight: Radius.circular(index == 2 ? 5 : 0),
+                  ),
+                  border: Border.all(
+                    color: Color.fromRGBO(216, 227, 255, 1),
+                  ),
                 ),
-                border: Border.all(
-                  color: Color.fromRGBO(216, 227, 255, 1),
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  "Sale",
-                  style: TextStyle(
-                    color: index == 0
-                        ? Constants.primaryColor
-                        : Color.fromRGBO(154, 154, 154, 1),
+                child: Center(
+                  child: Text(
+                    widget.items[index].title,
+                    style: TextStyle(
+                      color: activeIndex == 0
+                          ? Constants.primaryColor
+                          : Color.fromRGBO(154, 154, 154, 1),
+                    ),
                   ),
                 ),
               ),
