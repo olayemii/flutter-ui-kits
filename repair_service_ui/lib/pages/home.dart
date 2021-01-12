4da6835a26c01c2aafdc7cba5f36c397f8cf6340
Olayemii <donyemisco@gmail.com>
2021-01-12 16:33:59 +0100

    fix image

--- repair_service_ui/lib/pages/home.dart
+++ repair_service_ui/lib/pages/home.dart
@@ -3,12 +3,21 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        child: Stack(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+            ),
+            Container(
+              height: size.height,
+              decoration: BoxDecoration(),
+            )
+          ],
         ),
       ),
     );
