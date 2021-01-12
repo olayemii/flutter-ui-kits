56c8f1a7adc128476605f21db57b755ed0b90f3f
Olayemii <donyemisco@gmail.com>
2021-01-12 18:44:39 +0100

    add stack

--- repair_service_ui/lib/pages/request_service_flow.dart
+++ repair_service_ui/lib/pages/request_service_flow.dart
@@ -7,34 +7,69 @@ class RequestServiceFlow extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+      ),
       backgroundColor: Constants.primaryColor,
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SafeArea(child: SizedBox()),
-              Align(
-                alignment: Alignment.centerRight,
-                child: CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/dp.png"),
-                ),
-              ),
-              Row(
-                children: List.generate(
-                  3,
-                  (index) => Container(
-                    width: ScreenUtil().setWidth(9.0),
-                    height: ScreenUtil().setHeight(9.0),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/dp.png"),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
+                  Row(
+                    children: List.generate(
+                      3,
+                      (index) => Container(
+                        width: ScreenUtil().setWidth(9.0),
+                        height: ScreenUtil().setHeight(9.0),
+                        margin: EdgeInsets.only(right: 10.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color:
+                              Colors.white.withOpacity(index == 0 ? 1 : 0.56),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Hii, Uigiant",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.77),
+                      fontSize: 22.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "Choose your\ndevice",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w600,
+                      height: 1.2,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
