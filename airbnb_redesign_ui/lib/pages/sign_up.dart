25301725072e8efbf39599b7897340ce265977ea
Olayemii <donyemisco@gmail.com>
2021-01-14 00:57:48 +0100

    start signup page

---
+++ airbnb_redesign_ui/lib/pages/sign_up.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
