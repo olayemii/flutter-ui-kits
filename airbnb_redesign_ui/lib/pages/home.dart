8038ebff8e908e2fb83fd8bfda05a0134a2f9797
Olayemii <donyemisco@gmail.com>
2021-01-14 17:12:34 +0100

    add page controller

--- airbnb_redesign_ui/lib/pages/home.dart
+++ airbnb_redesign_ui/lib/pages/home.dart
@@ -2,13 +2,27 @@ import 'package:airbnb_redesign_ui/widgets/home_page_one.dart';
 import 'package:airbnb_redesign_ui/widgets/home_page_two.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   PageController controller = PageController();
+  @override
+  void initState() {
+    controller.addListener(() {
+      print("changed!");
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: PageView(
+          controller: controller,
           children: [
             HomePageOne(),
             HomePageTwo(),
