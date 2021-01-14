cf0f02caa0c7d893bc8410bafe9df41990334c9c
Olayemii <donyemisco@gmail.com>
2021-01-14 01:18:23 +0100

    fix styles

--- airbnb_redesign_ui/lib/pages/sign_up.dart
+++ airbnb_redesign_ui/lib/pages/sign_up.dart
@@ -7,6 +7,8 @@ import 'package:google_fonts/google_fonts.dart';
 class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -28,52 +30,64 @@ class SignUp extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Flexible(
-                  child: Column(
-                mainAxisSize: MainAxisSize.min,
-              )),
-              Column(
+        child: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              height: _size.height - Scaffold.of(context).appBarMaxHeight,
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  PrimaryButton(
-                    text: "Get Started",
-                    onPressed: () {},
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 0.5),
+                      ),
+                      width: double.infinity,
+                    ),
                   ),
-                  Center(
-                    child: Wrap(
-                      runAlignment: WrapAlignment.center,
-                      crossAxisAlignment: WrapCrossAlignment.center,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Text(
-                          "Already have account?",
-                          style: GoogleFonts.inter(
-                            fontSize: 14.0,
-                            color: Color.fromRGBO(64, 74, 106, 1),
-                          ),
-                        ),
-                        FlatButton(
+                        PrimaryButton(
+                          text: "Get Started",
                           onPressed: () {},
-                          child: Text(
-                            "Log In",
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Constants.primaryColor,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
                         ),
+                        Center(
+                          child: Wrap(
+                            runAlignment: WrapAlignment.center,
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            children: [
+                              Text(
+                                "Already have account?",
+                                style: GoogleFonts.inter(
+                                  fontSize: 14.0,
+                                  color: Color.fromRGBO(64, 74, 106, 1),
+                                ),
+                              ),
+                              FlatButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Log In",
+                                  style: GoogleFonts.inter(
+                                    fontSize: 14.0,
+                                    color: Constants.primaryColor,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   )
                 ],
-              )
-            ],
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
