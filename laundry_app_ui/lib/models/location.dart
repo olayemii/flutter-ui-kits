06646c0b717a5bba1b8d476354d8db974738f1fa
Olayemii <donyemisco@gmail.com>
2021-01-21 15:54:15 +0100

    complete location model

---
+++ laundry_app_ui/lib/models/location.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Location {
+  final String address;
+  final String state;
+  final Color color;
+  final String imagePath;
+
+  Location({this.address, this.color, this.imagePath, this.state});
+}
