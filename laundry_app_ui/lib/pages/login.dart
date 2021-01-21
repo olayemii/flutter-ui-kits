418963845ec4c910b03bf054e4ed5864f9ab9be7
Olayemii <donyemisco@gmail.com>
2021-01-21 14:28:28 +0100

    fix bg image

--- laundry_app_ui/lib/pages/login.dart
+++ laundry_app_ui/lib/pages/login.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:laundry_app_ui/utils/constants.dart';
-import 'package:laundry_app_ui/widgets/app_button.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Constants.primaryColor,
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [],
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      FlutterIcons.keyboard_backspace_mdi,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Log in to your account",
+                    style: Theme.of(context).textTheme.headline6.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
