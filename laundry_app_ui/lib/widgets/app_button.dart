2040382a0544234792d7bc9673bce350d841550e
Olayemii <donyemisco@gmail.com>
2021-01-21 14:10:21 +0100

    app button create

---
+++ laundry_app_ui/lib/widgets/app_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppButton extends StatelessWidget {
+  // Our primary button widget [to be reused]
+  final Function onPressed;
+  final String text;
+  final Color color;
+
+  AppButton({this.text, this.onPressed, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: this.onPressed,
+      child: Container(
+        width: double.infinity,
+        height: ScreenUtil().setHeight(50.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(12.0),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(169, 176, 185, 0.42),
+              spreadRadius: 0,
+              blurRadius: 8.0,
+              offset: Offset(0, 2),
+            )
+          ],
+        ),
+        child: Center(
+          child: Text(
+            this.text,
+            style: GoogleFonts.roboto(
+              color: Colors.white,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
