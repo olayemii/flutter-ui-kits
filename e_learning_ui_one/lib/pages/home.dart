c3283157781c0c19c1983c5aced0e65556977fd1
Olayemii <donyemisco@gmail.com>
2021-01-21 22:48:08 +0100

    set primary color

--- e_learning_ui_one/lib/pages/home.dart
+++ e_learning_ui_one/lib/pages/home.dart
@@ -1,9 +1,11 @@
+import 'package:e_learning_ui_one/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Constants.primaryColor,
       body: Container(),
     );
   }
---
+++ e_learning_ui_one/lib/utils/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final Color primaryColor = Color.fromRGBO(255, 89, 84, 1);
+}
