3b7f94107493511602e202a4ab20b53733d5575c
Olayemii <donyemisco@gmail.com>
2021-01-22 13:23:46 +0100

    add cards for courses

--- e_learning_ui_one/lib/pages/dashboard/dashboard.dart
+++ e_learning_ui_one/lib/pages/dashboard/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:e_learning_ui_one/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -38,69 +39,95 @@ class Dashboard extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 20.0,
-        ),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Hello Julia",
-                    style: TextStyle(
-                      color: Constants.secondaryColor,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    height: ScreenUtil().setHeight(48.0),
-                    width: ScreenUtil().setWidth(48.0),
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(225, 234, 255, 1),
-                      borderRadius: BorderRadius.circular(14.0),
-                    ),
-                    child: Image.asset("assets/images/dp.png"),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                width: 200.0,
-                child: RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "What do you want to learn today?",
-                        style: TextStyle(
-                          fontSize: 26.0,
-                          height: 1.5,
-                          color: Color.fromRGBO(13, 19, 51, 1),
-                          fontWeight: FontWeight.w600,
-                        ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 20.0,
+          ),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Hello Julia",
+                      style: TextStyle(
+                        color: Constants.secondaryColor,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500,
                       ),
-                      TextSpan(
-                        text: " 🔍",
-                        style: TextStyle(
-                          fontSize: 26.0,
-                          height: 1.5,
-                          color: Color.fromRGBO(13, 19, 51, 0.3),
-                          fontWeight: FontWeight.w600,
+                    ),
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      height: ScreenUtil().setHeight(48.0),
+                      width: ScreenUtil().setWidth(48.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(225, 234, 255, 1),
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      child: Image.asset("assets/images/dp.png"),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  width: 200.0,
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "What do you want to learn today?",
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            height: 1.5,
+                            color: Color.fromRGBO(13, 19, 51, 1),
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      )
-                    ],
+                        TextSpan(
+                          text: " 🔍",
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            height: 1.5,
+                            color: Color.fromRGBO(13, 19, 51, 0.3),
+                            fontWeight: FontWeight.w600,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+                Wrap(
+                  children: List.generate(
+                    4,
+                    (index) => Column(
+                      children: [
+                        index % 2 == 1
+                            ? SizedBox(
+                                height: 20.0,
+                              )
+                            : SizedBox.shrink(),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2.0 - 32.0,
+                          height: 200.0,
+                          margin: EdgeInsets.only(
+                              left: 5.0, right: 5.0, bottom: 5.0, top: 5.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            color: Colors.greenAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
