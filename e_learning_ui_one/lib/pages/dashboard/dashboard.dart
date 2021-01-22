cc56bd9e96e87692fc380576e3e220a9d477170f
Olayemii <donyemisco@gmail.com>
2021-01-22 11:27:54 +0100

    fix dashboard header

--- e_learning_ui_one/lib/pages/dashboard/dashboard.dart
+++ e_learning_ui_one/lib/pages/dashboard/dashboard.dart
@@ -1,13 +1,39 @@
+import 'package:e_learning_ui_one/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 20.0,
+        ),
         child: SafeArea(
           child: Column(
-            children: [],
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hello Julia",
+                    style: TextStyle(
+                      color: Constants.secondaryColor,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Container(
+                    height: ScreenUtil().setHeight(48.0),
+                    width: ScreenUtil().setWidth(48.0),
+                    decoration:
+                        BoxDecoration(color: Color.fromRGBO(r, g, b, opacity)),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- e_learning_ui_one/lib/utils/constants.dart
+++ e_learning_ui_one/lib/utils/constants.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static final Color primaryColor = Color.fromRGBO(255, 89, 84, 1);
+  static final Color secondaryColor = Color.fromRGBO(38, 87, 206, 1);
 }
