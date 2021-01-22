5ab9b4f9bb10a14a2606a21fb622d3a82ab6d489
Olayemii <donyemisco@gmail.com>
2021-01-22 14:15:52 +0100

    make course card

--- e_learning_ui_one/lib/utils/static_data.dart
+++ e_learning_ui_one/lib/utils/static_data.dart
@@ -1,4 +1,5 @@
 import 'package:e_learning_ui_one/models/course.dart';
+import 'package:e_learning_ui_one/widgets/course_card.dart';
 
 class StaticData {
   static final List<Course> courses = [
@@ -7,12 +8,28 @@ class StaticData {
       category: "Multimedia",
       percentage: 80.0,
       imagePath: "assets/images/multimedia.png",
+      type: CardType.PRIMARY,
     ),
     Course(
       name: "Python for everybody",
       category: "Programming",
-      percentage: 80.0,
-      imagePath: "assets/images/multimedia.png",
+      percentage: 60.0,
+      imagePath: "assets/images/programmer.png",
+      type: CardType.PLAIN,
+    ),
+    Course(
+      name: "Social Media Monitoring",
+      category: "Social Media",
+      percentage: 30.0,
+      imagePath: "assets/images/social.png",
+      type: CardType.PLAIN,
+    ),
+    Course(
+      name: "Fundamentals of Design",
+      category: "Graphic Design",
+      percentage: 60.0,
+      imagePath: "assets/images/designer.png",
+      type: CardType.SECONDARY_FADED,
     ),
   ];
 }
--- e_learning_ui_one/lib/widgets/course_card.dart
+++ e_learning_ui_one/lib/widgets/course_card.dart
@@ -5,6 +5,57 @@ enum CardType { PRIMARY, PLAIN, SECONDARY_FADED }
 class CourseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          index % 2 == 1
+              ? SizedBox(
+                  height: 20.0,
+                )
+              : SizedBox.shrink(),
+          Container(
+            width: MediaQuery.of(context).size.width / 2.0 - 32.0,
+            height: 200.0,
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 20.0,
+            ),
+            margin:
+                EdgeInsets.only(left: 5.0, right: 5.0, bottom: 5.0, top: 5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              color: Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CardTag(),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "Python for everybody",
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    height: 1.1,
+                    color: Color.fromRGBO(13, 19, 51, 1),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: LinearProgressIndicator(
+                    value: 0.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
