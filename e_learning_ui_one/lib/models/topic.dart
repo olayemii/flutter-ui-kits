cbe3b97caeb63f03865e13d4f5a3294c6bdd6aae
Olayemii <donyemisco@gmail.com>
2021-01-22 15:08:36 +0100

    add topic model

---
+++ e_learning_ui_one/lib/models/topic.dart
@@ -0,0 +1,9 @@
+class Topic {
+  final String title;
+  final String description;
+
+  Topic({
+    this.title,
+    this.description,
+  });
+}
--- e_learning_ui_one/lib/pages/single_course/single_course.dart
+++ e_learning_ui_one/lib/pages/single_course/single_course.dart
@@ -59,18 +59,71 @@ class SingleCourse extends StatelessWidget {
                 Container(
                   height: 186.0,
                   decoration: BoxDecoration(
-                      color: Constants.primaryColor,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(255, 89, 84, 0.38),
-                          offset: Offset(0, 4),
-                          blurRadius: 23.0,
-                          spreadRadius: 0.0,
-                        )
-                      ],
-                      image: DecorationImage()),
-                )
+                    color: Constants.primaryColor,
+                    borderRadius: BorderRadius.circular(12.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(255, 89, 84, 0.38),
+                        offset: Offset(0, 4),
+                        blurRadius: 23.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        "assets/images/multimediahd.png",
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Course Details",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Color.fromRGBO(13, 19, 51, 1),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 10.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(38, 87, 206, 0.1),
+                        borderRadius: BorderRadius.circular(7.0),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            FlutterIcons.clock_fea,
+                            size: 16.0,
+                            color: Constants.secondaryColor,
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            "3 hours, 20 min",
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Constants.secondaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
