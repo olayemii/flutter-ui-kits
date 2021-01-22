a6d9c93959542959aa3e3a39e097e2023c1dd7a8
Olayemii <donyemisco@gmail.com>
2021-01-22 14:10:37 +0100

    remove void callback

--- e_learning_ui_one/lib/utils/static_data.dart
+++ e_learning_ui_one/lib/utils/static_data.dart
@@ -1,5 +1,18 @@
 import 'package:e_learning_ui_one/models/course.dart';
 
 class StaticData {
-  static final List<Course> courses = [];
+  static final List<Course> courses = [
+    Course(
+      name: "Animation Basic Course",
+      category: "Multimedia",
+      percentage: 80.0,
+      imagePath: "assets/images/multimedia.png",
+    ),
+    Course(
+      name: "Python for everybody",
+      category: "Programming",
+      percentage: 80.0,
+      imagePath: "assets/images/multimedia.png",
+    ),
+  ];
 }
