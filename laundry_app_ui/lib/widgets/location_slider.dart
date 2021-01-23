69ea65ac88c64987092cbf7856a0c715eba22677
Olayemii <donyemisco@gmail.com>
2021-01-23 16:14:28 +0100

    final commit

--- laundry_app_ui/lib/widgets/location_slider.dart
+++ laundry_app_ui/lib/widgets/location_slider.dart
@@ -26,61 +26,66 @@ class LocationSlider extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         scrollDirection: Axis.horizontal,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            width: ScreenUtil().setWidth(200.0),
-            decoration: BoxDecoration(
-              color: locations[index].color,
-              borderRadius: BorderRadius.circular(8.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(169, 176, 185, 0.42),
-                  spreadRadius: 0,
-                  blurRadius: 8.0,
-                  offset: Offset(0, 2),
-                )
-              ],
-            ),
-            padding: EdgeInsets.symmetric(
-              vertical: 16.0,
-              horizontal: 12.0,
-            ),
-            child: Stack(
-              overflow: Overflow.visible,
-              children: [
-                Positioned(
-                  right: -16.0,
-                  top: 0.0,
-                  child: Opacity(
-                    opacity: 0.69,
-                    child: Image.asset(
-                      locations[index].imagePath,
-                      fit: BoxFit.fill,
+          return GestureDetector(
+            onTap: () {
+              print(locations[index]);
+            },
+            child: Container(
+              width: ScreenUtil().setWidth(200.0),
+              decoration: BoxDecoration(
+                color: locations[index].color,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(169, 176, 185, 0.42),
+                    spreadRadius: 0,
+                    blurRadius: 8.0,
+                    offset: Offset(0, 2),
+                  )
+                ],
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 12.0,
+              ),
+              child: Stack(
+                overflow: Overflow.visible,
+                children: [
+                  Positioned(
+                    right: -16.0,
+                    top: 0.0,
+                    child: Opacity(
+                      opacity: 0.69,
+                      child: Image.asset(
+                        locations[index].imagePath,
+                        fit: BoxFit.fill,
+                      ),
                     ),
                   ),
-                ),
-                RichText(
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "${locations[index].address},\n",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          height: 1.5,
-                          fontSize: 16.0,
+                  RichText(
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "${locations[index].address},\n",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            height: 1.5,
+                            fontSize: 16.0,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: locations[index].state,
-                        style: TextStyle(
-                          fontSize: 16.0,
+                        TextSpan(
+                          text: locations[index].state,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
