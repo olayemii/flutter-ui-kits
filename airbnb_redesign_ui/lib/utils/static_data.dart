ca7376d5ebc16fef82e434e7ea45a885e6b681f9
Amirul Sadikin <37245308+aiamirul@users.noreply.github.com>
2022-01-22 19:20:33 +0800

    Update static_data.dart

--- airbnb_redesign_ui/lib/utils/static_data.dart
+++ airbnb_redesign_ui/lib/utils/static_data.dart
@@ -4,6 +4,40 @@ import 'package:flutter/cupertino.dart';
 class StaticData {
   static final List<Property> properties = [
     Property(
+      name: "KL TENNIS TEAM LEAGUES:\\(25rd Nov)",
+      description:
+          "Johor is leading with 7th wins will they still hold their title!",
+      price: "\RMs.99",
+      imagePath: "assets/images/ten.jpg",
+    ),
+    Property(
+      name: "KERIS CONLAY:\FINALS\(25th Nov)",
+      description:
+          "Johor is leading with 7th wins will they still hold their title!",
+      price: "\RM69.99",
+      imagePath: "assets/images/rug.jpg",
+    ),
+    Property(
+      name: "TNB MALAYSIAN HOKEY LEAGUE:(Nov - Dec 2021)",
+      description:
+          "Johor is leading with 7th wins will they still hold their title!",
+      price: "\RM49.99",
+      imagePath: "assets/images/hok.jpg",
+    ),
+    Property(
+      name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
+      description:
+          "Johor is leading with 7th wins will they still hold their title!",
+      price: "\RM15.99",
+      imagePath: "assets/images/jvs.jpg",
+    ),
+    Property(
+      name: "Soneve Kiri",
+      description: "Are you searching for luxury hotels? This is for you !",
+      price: "\$220",
+      imagePath: "assets/images/house2.png",
+    ),
+    Property(
       name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
       description:
           "Johor is leading with 7th wins will they still hold their title!",
@@ -44,7 +78,7 @@ class StaticData {
       price: "\$220",
       imagePath: "assets/images/house2.png",
     ),
-     Property(
+    Property(
       name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
       description:
           "Johor is leading with 7th wins will they still hold their title!",
@@ -85,7 +119,7 @@ class StaticData {
       price: "\$220",
       imagePath: "assets/images/house2.png",
     ),
-     Property(
+    Property(
       name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
       description:
           "Johor is leading with 7th wins will they still hold their title!",
@@ -126,48 +160,7 @@ class StaticData {
       price: "\$220",
       imagePath: "assets/images/house2.png",
     ),
-     Property(
-      name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
-      description:
-          "Johor is leading with 7th wins will they still hold their title!",
-      price: "\RM99.99",
-      imagePath: "assets/images/jvs.jpg",
-    ),
     Property(
-      name: "KL TENNIS TEAM LEAGUES:\\(25rd Nov)",
-      description:
-          "Johor is leading with 7th wins will they still hold their title!",
-      price: "\RM59.99",
-      imagePath: "assets/images/ten.jpg",
-    ),
-    Property(
-      name: "KERIS CONLAY:\FINALS\(25th Nov)",
-      description:
-          "Johor is leading with 7th wins will they still hold their title!",
-      price: "\RM69.99",
-      imagePath: "assets/images/rug.jpg",
-    ),
-    Property(
-      name: "TNB MALAYSIAN HOKEY LEAGUE:(Nov - Dec 2021)",
-      description:
-          "Johor is leading with 7th wins will they still hold their title!",
-      price: "\RM49.99",
-      imagePath: "assets/images/hok.jpg",
-    ),
-    Property(
-      name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
-      description:
-          "Johor is leading with 7th wins will they still hold their title!",
-      price: "\RM15.99",
-      imagePath: "assets/images/jvs.jpg",
-    ),
-    Property(
-      name: "Soneve Kiri",
-      description: "Are you searching for luxury hotels? This is for you !",
-      price: "\$220",
-      imagePath: "assets/images/house2.png",
-    ),
-     Property(
       name: "Malaysia Super League:\Johor vs Selangor\(23rd Nov)",
       description:
           "Johor is leading with 7th wins will they still hold their title!",
